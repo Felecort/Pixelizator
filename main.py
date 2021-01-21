9cc44d8c9f74ce7af2c2f062ea2a97c7e07da7ad
FriLDD <dotpointen@gmail.com>
2021-01-21 22:48:39 +0300

    code refactor: all constants for frame/image pixelation are calculated by a separate function

--- main.py
+++ main.py
@@ -29,30 +29,47 @@ def draw_image(image, image_name, pixel_size):
     cv2.destroyAllWindows()
 
 
-# Converting an image to pixel art
-def conversion_to_pixel(image, pixel_size=15):
-    width = int(image.shape[1])
+def get_constant(image, pixel_size):
     height = int(image.shape[0])
-    # resolution of the final image
+    width = int(image.shape[1])
     pixel_height = int((height - pixel_size) / pixel_size) + 1
     pixel_width = int((width - pixel_size) / pixel_size) + 1
+
+    # returned constants
+    h_plus_ps1 = height - pixel_size + 1
+    w_min_ps1 = width - pixel_size + 1
     half_pixel_size = int(pixel_size / 2)
-    for y in range(0, height - pixel_size + 1, pixel_size):
-        for x in range(0, width - pixel_size + 1, pixel_size):
-            pixel_color = image[y + half_pixel_size, x + half_pixel_size]
+    pixel_h_mult_ps = pixel_height * pixel_size
+    pixel_w_mult_ps = pixel_width * pixel_size
+    #       0           1           2                  3            4
+    return h_plus_ps1, w_min_ps1, half_pixel_size, pixel_h_mult_ps, pixel_w_mult_ps
+
+
+def conversion_to_pixel(image, data, pixel_size=15):
+    # -------------------------------------------------------------
+    #                   h_plus_ps1
+    for y in range(0, data[0], pixel_size):
+        #                 w_min_ps1
+        for x in range(0, data[1], pixel_size):
+            #                   half_pixel_size         half_pixel_size
+            pixel_color = image[y + data[2], x + data[2]]
+            #       y_plus_ps           x_plus_ps
             image[y:y + pixel_size, x:x + pixel_size] = pixel_color
-    cropped_image = image[0:pixel_height * pixel_size, 0:pixel_width * pixel_size]
+            # pixel_h_mult_ps            pixel_w_mult_ps
+    cropped_image = image[0:data[3], 0:data[4]]
     return cropped_image
 
 
 # Pixelation of the video stream
 def pixel_video(video, pixel_size):
+    ret, frame = video.read()
+    data = get_constant(frame, pixel_size)
     while True:
         ret, frame = video.read()
         key = cv2.waitKey(1)
         if not ret or (key & 0xFF in [27, 32, 113]):
             break
-        image = conversion_to_pixel(frame, pixel_size)
+        image = conversion_to_pixel(frame, data, pixel_size)
         cv2.imshow("frame", image)
     video.release()
     cv2.destroyAllWindows()
@@ -66,7 +83,8 @@ def image_pixel_art():
         print('missing the input image')
         return -1
     image = cv2.imread(image_name)
-    pixel_img = conversion_to_pixel(image, pixel_size)
+    data = get_constant(image, pixel_size)
+    pixel_img = conversion_to_pixel(image, data, pixel_size)
     draw_image(pixel_img, image_name, pixel_size)
 
 
@@ -78,6 +96,7 @@ def video_pixel_art():
         print('missing the input video')
         return -1
     start = time()
+
     video = cv2.VideoCapture(video_name)
     fps = video.get(cv2.CAP_PROP_FPS)
     symbol_index = video_name.rfind('.')
@@ -85,12 +104,14 @@ def video_pixel_art():
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(video_name, fourcc, fps, (640, 360))
+    ret, frame = video.read()
+    data = get_constant(frame, pixel_size)
     while True:
         ret, frame = video.read()
         if not ret:
             break
         frame = cv2.flip(frame, 0)
-        image = conversion_to_pixel(frame, pixel_size)
+        image = conversion_to_pixel(frame, data, pixel_size)
         out.write(image)
     video.release()
     out.release()
