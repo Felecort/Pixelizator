9cc1f33e790fd4d7d9a540d0fa10bdabe9b68fe5
FriLDD <dotpointen@gmail.com>
2021-01-21 23:23:16 +0300

    rollback to previous commit
    de8f0b9c001b018beb6bf8ca5d1daf178dd603fd
    
    calculating constants in a separate function made the code work slower on 5.1%

--- main.py
+++ main.py
@@ -29,39 +29,30 @@ def draw_image(image, image_name, pixel_size):
     cv2.destroyAllWindows()
 
 
-def get_constant(image, pixel_size):
-    height = int(image.shape[0])
+# Converting an image to pixel art
+def conversion_to_pixel(image, pixel_size=15):
     width = int(image.shape[1])
+    height = int(image.shape[0])
+    # resolution of the final image
     pixel_height = int((height - pixel_size) / pixel_size) + 1
     pixel_width = int((width - pixel_size) / pixel_size) + 1
-    # returned constants
-    h_plus_ps1 = height - pixel_size + 1
-    w_min_ps1 = width - pixel_size + 1
     half_pixel_size = int(pixel_size / 2)
-    pixel_h_mult_ps = pixel_height * pixel_size
-    pixel_w_mult_ps = pixel_width * pixel_size
-    return h_plus_ps1, w_min_ps1, half_pixel_size, pixel_h_mult_ps, pixel_w_mult_ps
-
-
-def conversion_to_pixel(image, data, pixel_size=15):
-    for y in range(0, data[0], pixel_size):
-        for x in range(0, data[1], pixel_size):
-            pixel_color = image[y + data[2], x + data[2]]
+    for y in range(0, height - pixel_size + 1, pixel_size):
+        for x in range(0, width - pixel_size + 1, pixel_size):
+            pixel_color = image[y + half_pixel_size, x + half_pixel_size]
             image[y:y + pixel_size, x:x + pixel_size] = pixel_color
-    cropped_image = image[0:data[3], 0:data[4]]
+    cropped_image = image[0:pixel_height * pixel_size, 0:pixel_width * pixel_size]
     return cropped_image
 
 
 # Pixelation of the video stream
 def pixel_video(video, pixel_size):
-    ret, frame = video.read()
-    data = get_constant(frame, pixel_size)
     while True:
         ret, frame = video.read()
         key = cv2.waitKey(1)
         if not ret or (key & 0xFF in [27, 32, 113]):
             break
-        image = conversion_to_pixel(frame, data, pixel_size)
+        image = conversion_to_pixel(frame, pixel_size)
         cv2.imshow("frame", image)
     video.release()
     cv2.destroyAllWindows()
@@ -75,8 +66,7 @@ def image_pixel_art():
         print('missing the input image')
         return -1
     image = cv2.imread(image_name)
-    data = get_constant(image, pixel_size)
-    pixel_img = conversion_to_pixel(image, data, pixel_size)
+    pixel_img = conversion_to_pixel(image, pixel_size)
     draw_image(pixel_img, image_name, pixel_size)
 
 
@@ -88,24 +78,19 @@ def video_pixel_art():
         print('missing the input video')
         return -1
     start = time()
-
-    # Preparing constants
     video = cv2.VideoCapture(video_name)
     fps = video.get(cv2.CAP_PROP_FPS)
-    ret, frame = (video.read())
-    data = get_constant(frame, pixel_size)
-
-    # Codec settings and new file name
     symbol_index = video_name.rfind('.')
     video_name = video_name[:symbol_index] + "_EDIT.mp4"
+
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(video_name, fourcc, fps, (data[4], data[3]))
+    out = cv2.VideoWriter(video_name, fourcc, fps, (640, 360))
     while True:
         ret, frame = video.read()
         if not ret:
             break
         frame = cv2.flip(frame, 0)
-        image = conversion_to_pixel(frame, data, pixel_size)
+        image = conversion_to_pixel(frame, pixel_size)
         out.write(image)
     video.release()
     out.release()
