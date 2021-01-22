a7e70ae14eb0067a03284987be358b47ce5740f0
FriLDD <dotpointen@gmail.com>
2021-01-22 11:10:21 +0300

    the old function is cut out

--- main.py
+++ main.py
@@ -31,21 +31,6 @@ def draw_image(image, image_name, pixel_size):
     cv2.destroyAllWindows()
 
 
-# # Converting an image to pixel art
-# def conversion_to_pixel(image, pixel_size=15):
-#     width = int(image.shape[1])
-#     height = int(image.shape[0])
-#     # resolution of the final image
-#     pixel_height = int((height - pixel_size) / pixel_size) + 1
-#     pixel_width = int((width - pixel_size) / pixel_size) + 1
-#     half_pixel_size = int(pixel_size / 2)
-#     for y in range(0, height - pixel_size + 1, pixel_size):
-#         for x in range(0, width - pixel_size + 1, pixel_size):
-#             pixel_color = image[y + half_pixel_size, x + half_pixel_size]
-#             image[y:y + pixel_size, x:x + pixel_size] = pixel_color
-#     cropped_image = image[0:pixel_height * pixel_size, 0:pixel_width * pixel_size]
-#     return cropped_image
-
 # Converting an image to pixel art
 def conversion_to_pixel(image, pixel_size=15):
     width = int(image.shape[1])
